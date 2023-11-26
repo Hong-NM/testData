b121089cb78e8d99b0714ee94e01a85dda426c34
Nguyễn Minh Hồng <121294428+Hong-NM@users.noreply.github.com>
2023-11-26 21:03:15 +0700

    Update sort.py by add new comment

--- sort.py
+++ sort.py
@@ -3,6 +3,7 @@ import random
 random.seed(1234)
 data = [random.randint(1, 10000) for _ in range(10000)]
 #print(data)
+#test comment
 with open('./output.txt', 'w') as f:
     for number in data:
         f.write(str(number) + ',')
